c0394b05b91e47d8fb5adc0a56b1daf05fc4129f
Helion <huyhjh@gmail.com>
2019-03-10 02:19:50 +0900

    Update and rename datadog-apm-config-script.sh to datadog-php-apm-config-script.sh

--- datadog/datadog-php-apm-config-script.sh
+++ datadog/datadog-php-apm-config-script.sh
@@ -5,8 +5,6 @@ sed -i 's/# apm_config:.*/apm_config:/' /etc/datadog-agent/datadog.yaml
 sed -i 's/#   enabled: true/  enabled: true/' /etc/datadog-agent/datadog.yaml 
 sed -i 's/#   env: none/  env: php/' /etc/datadog-agent/datadog.yaml 
 
- 
-
 ## dd-trace 다운 후 rpm파일 설치 
 wget https://github.com/DataDog/dd-trace-php/releases/download/0.13.0/datadog-php-tracer-0.13.0_beta-1.x86_64.rpm 
 sudo rpm -ivh ~/datadog-php-tracer-0.13.0_beta-1.x86_64.rpm 
